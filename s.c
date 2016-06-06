93d3d0f2001c5b579ca414772c217253bdd709db
suguna18 <spreethi836@gmail.com>
2016-06-06 11:46:07 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int ele;
+	scanf("%d",&ele);
+	if(ele>0)
+    {
+    	printf("it is positive");
+    }	
+    else if(ele<0)
+    {
+    	printf("it is negative");
+    }
+    else
+    {
+    	printf("it is Zero");
+    }
+}
